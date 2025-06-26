8cb03a889a828900be01628586f2dad045365346
Ambrose <ambroseocholao@gmail.com>
2025-06-26 19:49:05 +0100

    New models and its full SQL migration
    - Created new a new model for the user. This is the first temporary version.
    
    - Fully migrated the changes made for the new model and reconcilidated them for the SQL database (if I used the terminology correctly).
    
    - Wrote some SQL commands to add in two rows of dummy data for the User.
    
    - Full migrations generated subsequent migration .py files. Have two for the two attempts made with full migrations.

--- enhanced_access_portal/login_page/models.py
+++ enhanced_access_portal/login_page/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class User(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  password = models.CharField(max_length=255)
+  joined_date = models.DateField(default = date.today) # https://www.geeksforgeeks.org/datefield-django-models/
+  user_type = models.CharField(max_length=255)
