e43d4a742d321fd5830253690f006812a203c87c
Ambrose <ambroseocholao@gmail.com>
2025-06-27 10:59:32 +0100

    Alternate login options UI + Json HTTP Requests
    - Added a new "alternate_login_options" CSS class that is used to centre alternate login buttons on the login page.
    
    - Adjusted "login_button" CSS class so that it can accomodate a loading gif icon sitting on top of it without affecting its positioning.
    
    - Added a "alternate_login_button" CSS class and associated buttons for "Forgot Password" and "Register" to make the user interface better. Using this instead of just hyperlinks.
    
    - Added "button_wrapper" CSS class to wrap the login button and loading gif icon into same position.
    
    - Added "loader" CSS class to describe a loading icon to sit on top of the login button for later usage.
    
    - Got working JSON POST requests after consulting Chat GPT. So clicking the Login button sends a POST, and the server returns a JsonResponse
    
    - Made the default URL page pass the login page html template. However it would be best to simply just redirect the user to the login page.

--- enhanced_access_portal/login_page/urls.py
+++ enhanced_access_portal/login_page/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('login_page/', views.login_page_view, name='login_page_view'),
+    path('', views.login_page_view, name='login_page_view'),
+    path('login_request/', views.USER_PROMPT_login_attempt, name="login_request")
 ]
--- enhanced_access_portal/login_page/views.py
+++ enhanced_access_portal/login_page/views.py
@@ -1,7 +1,20 @@
 
 from django.http import HttpResponse
 from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 def login_page_view(request):
     template = loader.get_template('login_webpage.html')
     return HttpResponse(template.render())
+
+@csrf_exempt
+def USER_PROMPT_login_attempt(request):
+    if request.method == "POST":
+        user_input = request.POST.get("Output1")
+        user_input2 = request.POST.get("Output2")
+        print(request)
+        print(user_input)
+        print(user_input2)
+        return JsonResponse({"status": "success", "message": f"Received: {user_input}"})
+    return JsonResponse({"status": "fail", "message": "Only POST allowed"}, status=405)
