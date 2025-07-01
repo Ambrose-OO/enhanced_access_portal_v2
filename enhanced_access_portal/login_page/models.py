e9a8f35c78a44e8f122f40f07848f6acc3fc3a1c
Ambrose <ambroseocholao@gmail.com>
2025-07-01 20:50:58 +0100

    Email address added to User model
    - Edited the "User" model to have the email address field which has a defualt value. This is to enable users to login using their email rather than name.
    
    - Created User model migration, then fully migrated it over. So that the sqlite3 database structure can be modified for use.

--- enhanced_access_portal/login_page/models.py
+++ enhanced_access_portal/login_page/models.py
@@ -5,6 +5,7 @@ from datetime import date
 class User(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
+  emailaddress = models.CharField(max_length=255, default = "example@example.com")
   password = models.CharField(max_length=255)
   joined_date = models.DateField(default = date.today) # https://www.geeksforgeeks.org/datefield-django-models/
   user_type = models.CharField(max_length=255)
