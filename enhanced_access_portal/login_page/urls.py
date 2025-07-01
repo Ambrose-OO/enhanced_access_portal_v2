a7bb0d11a02bf41040db097b0d3ddb277a8f5071
Ambrose <ambroseocholao@gmail.com>
2025-07-01 20:54:39 +0100

    Register request & SQL Account creation
    - Created a new URL a user can POST to in order to submit an account registration request.
    
    - Worked on handling account creation on the backend in views.py. Account created and data stored in sqlite database.
    
    - Front end javascript additions and tweaks to structure of the webpage on the login_webpage.html file to enable account creation requesting.

--- enhanced_access_portal/login_page/urls.py
+++ enhanced_access_portal/login_page/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('login_page/', views.login_page_view, name='login_page_view'),
     path('', views.login_page_view, name='login_page_view'),
-    path('login_request/', views.USER_PROMPT_login_attempt, name="login_request")
+    path('login_request/', views.USER_PROMPT_login_attempt, name="login_request"),
+    path('register_request/', views.USER_PROMPT_register_attempt, name="register_request")
 ]
--- enhanced_access_portal/login_page/views.py
+++ enhanced_access_portal/login_page/views.py
@@ -4,6 +4,8 @@ from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
+from login_page.models import User
+
 def login_page_view(request):
     template = loader.get_template('login_webpage.html')
     return HttpResponse(template.render())
@@ -18,3 +20,30 @@ def USER_PROMPT_login_attempt(request):
         print(user_input2)
         return JsonResponse({"status": "success", "message": f"Received: {user_input}"})
     return JsonResponse({"status": "fail", "message": "Only POST allowed"}, status=405)
+
+@csrf_exempt
+def USER_PROMPT_register_attempt(request):
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        other_names = request.POST.get("other_names")
+
+        email_address = request.POST.get("email_address")
+        password = request.POST.get("password")
+
+       
+        user = User(
+            firstname = first_name, 
+            lastname = other_names, 
+            emailaddress = email_address, 
+            password = password, 
+            user_type="USER"
+        )
+        
+        user.save()
+        return JsonResponse({"status": "success", "message": "Account registered"})
+        #except:
+        #     return JsonResponse({"status": "fail", "message": "Registration process failed"})
+        
+    return JsonResponse({"status": "fail", "message": "Only POST allowed"}, status=405)
+
+
