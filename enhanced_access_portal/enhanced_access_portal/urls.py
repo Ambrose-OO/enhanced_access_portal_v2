3dae60bf3c9c2c7a229eaa1771a3ae8a224c8f97
Ambrose <ambroseocholao@gmail.com>
2025-10-02 13:58:41 +0100

    Project .ico file for the browser tab

--- enhanced_access_portal/enhanced_access_portal/urls.py
+++ enhanced_access_portal/enhanced_access_portal/urls.py
@@ -17,11 +17,14 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.conf.urls import url
 
 urlpatterns = [
     path('', include('login_page.urls')),
     path('', include('resource_dashboard.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/media/dashboard.ico'))
 ]
 
 if settings.DEBUG:
