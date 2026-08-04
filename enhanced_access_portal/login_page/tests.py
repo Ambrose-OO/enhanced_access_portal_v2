634ab4ad64d0e45f800d7079c3c10f558694a68a
Ambrose-OO <73991515+Ambrose-OO@users.noreply.github.com>
2026-08-04 21:42:46 +0100

    Addition - Initial unit test Login_Page
    - Added for initial testing purposes to test if Github Actions will work

--- enhanced_access_portal/login_page/tests.py
+++ enhanced_access_portal/login_page/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class LoginPageTests(TestCase):
+    def test_login_page_loads(self):
+        response = self.client.get('/login_page/')
+        self.assertEqual(response.status_code, 200)
