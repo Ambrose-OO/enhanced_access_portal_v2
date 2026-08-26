f559f6698e0c8e4cfe62edce3d407a703ed8ce5d
Ambrose-OO <73991515+Ambrose-OO@users.noreply.github.com>
2026-08-26 14:14:08 +0100

    Project listings: Ammended server logs

--- enhanced_access_portal/resource_dashboard/views.py
+++ enhanced_access_portal/resource_dashboard/views.py
@@ -409,27 +409,18 @@ def USER_ADMIN_PROMPT_project_listings(request):
         debug_id = request.POST.get("debug_id")
 
         print()
-        print("----------------")
-        if debug_id:
-            print(debug_id + " Project listings")
-            
-        else:
-            print("----------------")
-            print("Project listings")
-            print("----------------")
-
-            
                 
         logged_in_status = request.session.get("logged_in")
         user_type = request.session.get("user_type")
         user_id = request.session.get("user_id")  
         user = fetch_user_from_id(user_id)
 
-        print(debug_id + " Server: Point B - Project listings")
+        print(debug_id + " Server: Point B - Project listings\n")
 
         if (logged_in_status == True) and (user):
             if (user_type == "USER"):
-                print(debug_id + " Server: Point B.1.1-User - Project listings")
+                print(debug_id + " Server: Point B.1.1-User - Project listings\n")
+
                 # Variable to store data on projects that the user is allowed to see
                 user_project_listings = []
                 
@@ -443,7 +434,7 @@ def USER_ADMIN_PROMPT_project_listings(request):
                             project_detail = fetch_project_details(project)    
                             user_project_listings.append(project_detail)
 
-                print(debug_id + " Server: Point B.1.2-User - Project listings")
+                print(debug_id + " Server: Point B.1.2-User - Project listings\n")
 
                 # Returning project data in JSON format back to the user
                 return JsonResponse(
@@ -455,7 +446,7 @@ def USER_ADMIN_PROMPT_project_listings(request):
                 ) 
             elif (user_type == "ADMIN"):
 
-                print(debug_id + " Server: Point B.2.1-Admin - Project listings")
+                print(debug_id + " Server: Point B.2.1-Admin - Project listings\n")
 
                 # Variable to store all data on projects for the admin to see
                 admin_project_listings = collate_ADMIN_project_listings()
@@ -1030,20 +1021,13 @@ def ADMIN_USER_PROMPT_all_vms(request):
         debug_id = request.POST.get("debug_id")
 
         print()
-        print("-----------------------")
-        if debug_id:
-            print(debug_id + " Revealing all VMS")
-        else:
-            print("Revealing all VMS")
-        print("-----------------------")
-
-
-        print(debug_id + " Server: Point B")
+       
+        print(debug_id + " Server: Point B - All vms\n")
         logged_in_status = request.session.get("logged_in")
     
         if (logged_in_status == True):
 
-            print(debug_id + " Server: Point C")
+            print(debug_id + " Server: Point C - All vms\n")
 
             vms_list = []
 
@@ -1056,9 +1040,7 @@ def ADMIN_USER_PROMPT_all_vms(request):
                 vm_detail["vm_ip"] = vm.vm_ip 
                 vms_list.append(vm_detail) 
 
-            print(debug_id + " Server: Point D")
-
-            print("-----------------")
+            print(debug_id + " Server: Point D - All vms\n")
 
             return JsonResponse(
                 {
